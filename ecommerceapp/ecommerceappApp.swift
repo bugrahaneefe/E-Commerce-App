972149127a4c30d1791d6ad3878e501b7de44c7b
bugrahaneefe <befe19@ku.edu.tr>
2023-08-01 09:54:20 +0300

    Initial Commit

---
+++ ecommerceapp/ecommerceappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerceappApp.swift
+//  ecommerceapp
+//
+//  Created by Buğrahan Efe on 1.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ecommerceappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
